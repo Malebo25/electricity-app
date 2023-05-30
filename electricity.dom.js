fb60bc02706f2d263cdb048da705ab1c3486beb4
Malebo25 <malebomotlhake26@gmail.com>
2023-05-30 14:14:41 +0200

    testing, Dom, local storage initial commits

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,33 @@
 // DOM element(s) references
+const radioBTN = document.querySelector(".topup");
+const buyBTN = document.querySelector(".topupNow");
+const availableUnits = document.querySelector(".unitsAvailable");
+const spentAmount = document.querySelector(".totalAmount");
 
+// Factory Function instance
+const electricity = Electricity(Number(localStorage.getItem("UnitsNow")),Number(localStorage.getItem("SpentAmount")));
+availableUnits.innerHTML = electricity.getUnitsAvailable();
+localStorage.getItem("UnitsNow");
 
+spentAmount.innerHTML = electricity.totalAmountSpent();
+localStorage.getItem("SpentAmount");
+// DOM events here
+buyBTN.addEventListener("click", function () {
+  var checkedradioBtn = document.querySelector(
+    "input[name='buyElectricity']:checked"
+  );
+  let Availamount = checkedradioBtn.value;
+  if (Availamount !== "advance") {
+    Availamount = Number(checkedradioBtn.value);
 
-// Factory Function instance 
-const electricity =  Electricity();
+    electricity.topUpElectricity(Availamount);
+    availableUnits.innerHTML = electricity.getUnitsAvailable();
+  } else {
+    electricity.topUpElectricity(Availamount);
+    availableUnits.innerHTML = electricity.getUnitsAvailable();
+  }
+  spentAmount.innerHTML = electricity.totalAmountSpent();
 
-
-// DOM events here 
+  localStorage.setItem("UnitsNow", Number(electricity.getUnitsAvailable()));
+  localStorage.setItem("SpentAmount", Number(electricity.totalAmountSpent()));
+});
--- electricity.js
+++ electricity.js
@@ -1,46 +1,119 @@
-function Electricity() {
+function Electricity(availUnits,spent) {
+  let unitsAvailable = availUnits||0;
+  let electricityUsage = {
+    R10: 0,
+    R50: 0,
+    R20: 0,
+  };
+  let amountSpent = spent||0;
 
-    // do we want to go with this or array? 
-    let appliances = {
-        'Stove': 10, 
-        'Kettle': 5, 
-        'TV': 3, 
-        'Fridge': 13
-    };
+  // do we want to go with this or array?
+  let appliances = {
+    Stove: 10,
+    Kettle: 5,
+    TV: 3,
+    Fridge: 13,
+  };
 
-    function topUpElectricity(amount) {
-
-
+  function topUpElectricity(amount) {
+    if (amount === 10) {
+      unitsAvailable += 7;
+      electricityUsage["R10"]++;
+      amountSpent += 10;
+    } else if (amount === 20) {
+      unitsAvailable += 14;
+      amountSpent += 20;
+      electricityUsage["R20"]++;
+    } else if (amount === 50) {
+      unitsAvailable += 35;
+      amountSpent += 50;
+      electricityUsage["R50"]++;
+    } else if (amount === "advance") {
+      if (electricityUsage["advance"] === undefined) {
+        unitsAvailable += 21;
+        electricityUsage["advance"] = 1;
+      } else {
+        return false;
+      }
     }
+  }
 
-    function getUnitsAvailable() {
-         return unitsAvailable;
+  /*
+   * return true and substract from unit available if there is enough units to use the appliance
+   * other wise return false and do nothing.
+   */
+  function useAppliance(appliance) {
+    if (unitsAvailable >= 10) {
+
+      if (appliance === "Stove") {
+        unitsAvailable -= appliances["Stove"];
+        return true;
+      }
+      else {
+        return false;
+      }
+      
     }
-
-    /*
-    * return true and substract from unit available if there is enough units to use the appliance
-    * other wise return false and do nothing.
-    */
-    function useAppliance(appliance) {
+    else if (unitsAvailable >= 13) {
+      
+      if (appliance === "Fridge") {
+        unitsAvailable -= appliances["Fridge"];
+        return true;
         
+      }
+      else {
+        return false
+      }
     }
 
-    function advanceTaken() {
-    }
-
-    function totalAmountSpent() {
-    }
-
-    function totalUnitsBought(){
-    }
-
-    return {
-        advanceTaken,
-        topUpElectricity,
-        getUnitsAvailable,
-        useAppliance,
-        totalAmountSpent,
-        totalUnitsBought
+    else if (unitsAvailable >= 5) {
+      if (appliance === "Kettle") {
+        unitsAvailable -= appliances["Kettle"];
+        return true;
+      } else {
+        return false;
+      }
 
     }
+    else if (unitsAvailable >= 3) {
+      if (appliance === "TV") {
+        unitsAvailable -= appliances["TV"];
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
+      
+    
+  }
+
+  function getUnitsAvailable() {
+    return unitsAvailable;
+  }
+  function advanceTaken() {
+    if (electricityUsage["advance"]) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  function totalAmountSpent() {
+    return amountSpent;
+  }
+
+  function totalUnitsBought() {
+
+  }
+
+  return {
+    advanceTaken,
+    topUpElectricity,
+    getUnitsAvailable,
+    useAppliance,
+    totalAmountSpent,
+    totalUnitsBought,
+  };
 }
